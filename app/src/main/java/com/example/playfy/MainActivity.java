a7034c9ea47aa0065b2ce3906286cf9b34e35031
souvik757 <88132246+souvik757@users.noreply.github.com>
2023-03-28 17:20:33 +0530

    This is a UI update

--- app/src/main/java/com/example/playfy/MainActivity.java
+++ app/src/main/java/com/example/playfy/MainActivity.java
@@ -6,13 +6,24 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView _logo_ ;
+    ImageView _load_ ;
+    TextView _txt_ ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _logo_ = findViewById(R.id.imageView4) ;
+        _load_ = findViewById(R.id._load_) ;
+        _txt_ = findViewById(R.id.application_name) ;
+        _logo_.setAnimation(AnimationUtils.loadAnimation(this , R.anim._fade_));
+        _txt_.setAnimation(AnimationUtils.loadAnimation(this , R.anim._blink_));
+        _load_.setAnimation(AnimationUtils.loadAnimation(this , R.anim.rotate));
 
         new Handler().postDelayed(() -> {
             MediaPlayer.create(this , R.raw.welcome).start();
